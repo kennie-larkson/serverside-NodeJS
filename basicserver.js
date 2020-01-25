c99c076e54587890c3492b2dcccbdacffc370dc5
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-25 07:47:57 +0100

    ADDED COMMENTS

--- basicserver.js
+++ basicserver.js
@@ -1,13 +1,16 @@
 // BASIC NODE SERVER
 
+//REQUIRE THE NODE HTTP MODULE
 const http = require('http');
 
+//CREATE THE SERVER USING THE HTTP OBJECT
 const server = http.createServer((req, res)=>{
 
     res.setHeader("Content-type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.writeHead(200); // status code HTTP 200/OK
 
+    //DATA TO BE SENT TO CLIENT/BROWSER
     const dataObj = {id:1111, name:"kennie", email:"kennie@my.org"}
     const data = JSON.stringify(dataObj);
 
@@ -15,7 +18,7 @@ const server = http.createServer((req, res)=>{
     res.end(data);
 });
 
-
+//HAVE THE SERVER LISTEN TO CLIENT REQUESTS ON ANY AVAILABLE PORT ON THE COMPUTER
 server.listen(1234,()=>{
     console.log('Server running on port: 1234');
 });
